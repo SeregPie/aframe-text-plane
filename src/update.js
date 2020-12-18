b67f8c0d49475df63b2a63aec6b730824348acf8
Sergej Sintschilin <seregpie@gmail.com>
2020-12-18 14:02:26 +0100

    check if object exists

--- src/update.js
+++ src/update.js
@@ -1,50 +1,50 @@
 import name from './name';
 
 export default function(oldData) {
-	let {
-		data,
-		el,
-	} = this;
-	let {
-		alignment,
-		backgroundColor,
-		color,
-		fog,
-		fontFamily,
-		fontSize,
-		fontStyle,
-		fontVariant,
-		fontWeight,
-		lineGap,
-		opacity,
-		padding,
-		strokeColor,
-		strokeWidth,
-		text,
-		transparent,
-	} = data;
+	let {el} = this;
 	let object = el.getObject3D(name);
-	if (Object.keys(oldData).length) {
-		Object.assign(object, {
+	if (object) {
+		let {data} = this;
+		let {
 			alignment,
 			backgroundColor,
 			color,
+			fog,
 			fontFamily,
 			fontSize,
 			fontStyle,
 			fontVariant,
 			fontWeight,
 			lineGap,
+			opacity,
 			padding,
 			strokeColor,
 			strokeWidth,
 			text,
+			transparent,
+		} = data;
+		if (Object.keys(oldData).length) {
+			Object.assign(object, {
+				alignment,
+				backgroundColor,
+				color,
+				fontFamily,
+				fontSize,
+				fontStyle,
+				fontVariant,
+				fontWeight,
+				lineGap,
+				padding,
+				strokeColor,
+				strokeWidth,
+				text,
+			});
+		}
+		let {material} = object;
+		Object.assign(material, {
+			fog,
+			opacity,
+			transparent,
 		});
 	}
-	let {material} = object;
-	Object.assign(material, {
-		fog,
-		opacity,
-		transparent,
-	});
 }
