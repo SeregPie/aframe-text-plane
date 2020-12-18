b4ebee1da6e159e76d857670677560d9c7dfc446
Sergej Sintschilin <seregpie@gmail.com>
2020-12-18 14:41:50 +0100

    update demo

--- demo/script.js
+++ demo/script.js
@@ -2,110 +2,8 @@
 
 	Vue.config.ignoredElements = [/^a-/];
 
-	let VColorInput = {
-		model: {
-			prop: 'modelValue',
-			event: 'update:modelValue',
-		},
-		props: {
-			label: {},
-			modelValue: {},
-			noAlpha: {
-				type: Boolean,
-				default: false,
-			},
-		},
-		data() {
-			return {
-				menu: false,
-			};
-		},
-		computed: {
-			color() {
-				let {value} = this;
-				return chroma(value).css();
-			},
-			value: {
-				get() {
-					let {
-						modelValue,
-						noAlpha,
-					} = this;
-					let [r, g, b, a] = chroma(modelValue).rgba();
-					a = (a > 0) ? a : Number.EPSILON; // vuetify bug
-					return noAlpha ? {r, g, b} : {r, g, b, a};
-				},
-				set(value) {
-					let [r, g, b, a] = chroma(value).rgba();
-					a = (a > Number.EPSILON) ? a : 0; // vuetify bug
-					this.$emit('update:modelValue', chroma({r, g, b, a}).css());
-				},
-			},
-		},
-		template: '#VColorInput',
-	};
-
-	let VCustomSlider = {
-		model: {
-			prop: 'modelValue',
-			event: 'update:modelValue',
-		},
-		props: {
-			label: {},
-			max: {},
-			min: {},
-			modelValue: {},
-		},
-		computed: {
-			value: {
-				get() {
-					return this.modelValue;
-				},
-				set(value) {
-					this.$emit('update:modelValue', value);
-				},
-			},
-		},
-		template: '#VCustomSlider',
-	};
-
-	let VIndexSlider = {
-		components: {VCustomSlider},
-		model: {
-			prop: 'modelValue',
-			event: 'update:modelValue',
-		},
-		props: {
-			items: {},
-			label: {},
-			modelValue: {},
-		},
-		computed: {
-			item() {
-				return this.items[this.itemIndex];
-			},
-			itemIndex: {
-				get() {
-					return this.modelValue;
-				},
-				set(value) {
-					this.$emit('update:modelValue', value);
-				},
-			},
-			itemLabel() {
-				let {item} = this;
-				return item.label ?? item;
-			},
-			itemValue() {
-				let {item} = this;
-				return item.value ?? item;
-			},
-		},
-		template: '#VIndexSlider',
-	};
-
 	new Vue({
-		el: '#app',
+		el: '#App',
 		vuetify: new Vuetify(),
 		data() {
 			let text = [
@@ -132,7 +30,7 @@
 				backgroundColor: chroma('#073b4c').alpha(1/3).css(),
 				color: '#ffd166',
 				drawer: true,
-				fontFamily: JustMyLuck.pick(fontFamilyValues),
+				fontFamily: chance.pickone(fontFamilyValues),
 				fontFamilyValues,
 				fontSizeIndex: 1,
 				fontSizeValues: [1, 2, 3],
@@ -154,25 +52,41 @@
 				return this.fontSizeValues[this.fontSizeIndex];
 			},
 			fontSizeDisplayString() {
-				return Number(this.fontSize).toLocaleString('en', {maximumFractionDigits: 2});
+				return Number(this.fontSize).toLocaleString('en', {
+					maximumFractionDigits: 2,
+				});
 			},
 			lineGap() {
 				return this.lineGapValues[this.lineGapIndex];
 			},
 			lineGapDisplayString() {
-				return Number(this.lineGap).toLocaleString('en', {style: 'percent'});
+				return Number(this.lineGap).toLocaleString('en', {
+					style: 'percent',
+				});
 			},
 			padding() {
 				return this.paddingValues[this.paddingIndex];
 			},
 			paddingDisplayString() {
-				return Number(this.padding).toLocaleString('en', {style: 'percent'});
+				return Number(this.padding).toLocaleString('en', {
+					style: 'percent',
+				});
 			},
 			strokeWidth() {
 				return this.strokeWidthValues[this.strokeWidthIndex];
 			},
 			strokeWidthDisplayString() {
-				return Number(this.strokeWidth).toLocaleString('en', {style: 'percent'});
+				return Number(this.strokeWidth).toLocaleString('en', {
+					style: 'percent',
+				});
+			},
+		},
+		methods: {
+			randomizeFontFamily() {
+				let {fontFamilyValues} = this;
+				Object.assign(this, {
+					fontFamily: chance.pickone(fontFamilyValues),
+				});
 			},
 		},
 		components: {
@@ -229,20 +143,15 @@
 											fov: 60,
 										}),
 										'orbit-controls': AFRAME.utils.styleParser.stringify({
-											dampingFactor: 1/16,
-											enableKeys: false,
-											initialPosition: (([x, y, z]) => AFRAME.utils.coordinates.stringify({x, y, z}))([0, 0, 32]),
-											maxDistance: 128,
-											minDistance: 2,
-											panSpeed: 1,
-											rotateSpeed: 1/6,
-											zoomSpeed: 1/2,
+											initialPosition: (([x, y, z]) =>
+												AFRAME.utils.coordinates.stringify({x, y, z})
+											)([0, 0, 32]),
 										}),
 										'look-controls': AFRAME.utils.styleParser.stringify({
 											enabled: false,
 										}),
 									},
-									key: 'pkztceqjwmzw',
+									key: 0,
 								},
 							),
 							h(
@@ -262,15 +171,13 @@
 											text,
 										}),
 									},
-									key: 'pjuqezmebnjt',
+									key: 1,
 								},
 							),
 						],
 					);
 				},
 			},
-			VColorInput,
-			VIndexSlider,
 		},
 	});
 })();
